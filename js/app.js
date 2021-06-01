2296ddde3d9ca2db1bc342a6d092662df250cde8
Gonzalo De Castro <gonzalodecastro1@gmail.com>
2021-06-01 14:10:28 -0300

    Se quita tablero en HTML y se incorpora desde javascript pero sin fichas y básico

---
+++ js/app.js
@@ -0,0 +1,16 @@
+const tamano = 8;
+var tablero = document.getElementById("tablero");
+for (let i=0; i<tamano; i++) {
+
+    // creamos la fila
+    let fila=document.createElement("div");
+    fila.classList.add("fila")
+    for (let j=0; j<tamano; j++) {
+
+        // creamos cada elemento de la fila
+        let div=document.createElement("div");
+        div.classList.add("recuadro")
+        fila.appendChild(div);
+    }
+    tablero.appendChild(fila);
+}
