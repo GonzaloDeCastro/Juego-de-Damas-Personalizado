5c1b577a25a070631e6e295d614c1adc271ae6a4
Gonzalo De Castro <gonzalodecastro1@gmail.com>
2021-06-02 19:05:50 -0300

    Se crea una nueva tabla pero hecah con arreglos para asi poder acceder a cada uno de los recuadros

--- js/app.js
+++ js/app.js
@@ -1,17 +1,44 @@
-
-var tamano = 8;
 var tablero = document.getElementById("tablero");
-for (var i=0; i<tamano; i++) {
+tamano = 8;
+var arregloTablero = [[1,2,3,4,5,6,7,8],[1,2,3,4,5,6,7,8],[1,2,3,4,5,6,7,8],
+                [1,2,3,4,5,6,7,8],[1,2,3,4,5,6,7,8],[1,2,3,4,5,6,7,8],
+                [1,2,3,4,5,6,7,8],[1,2,3,4,5,6,7,8]];
+                
 
-    // creamos la fila
-    var fila=document.createElement("div");
+for (var i=0; i<tamano; i++) {
+    var fila  =document.createElement("cuadro");
     fila.classList.add("fila")
     for (var j=0; j<tamano; j++) {
-
-        // creamos cada elemento de la fila
-        var div=document.createElement("div");
-        div.classList.add("recuadro")
-        fila.appendChild(div);
-    }
+        if(i%2 == 0){
+            if(j%2 == 0){
+                var cuadro = document.createElement("cuadro");
+                    cuadro.classList.add("recuadroBlanco");
+                    fila.appendChild(cuadro);
+                console.log('fila par recuadro blanco '+arregloTablero[i][j]);
+                
+            }
+            else{
+                var cuadro = document.createElement("cuadro");
+                    cuadro.classList.add("recuadroNegro");
+                    fila.appendChild(cuadro);
+                console.log('fila par recuadro negro '+arregloTablero[i][j]);
+            }
+        }
+        else{
+            if(j%2 == 0){
+                var cuadro = document.createElement("cuadro");
+                    cuadro.classList.add("recuadroNegro");
+                    fila.appendChild(cuadro);
+                console.log('fila impar recuadro negro '+arregloTablero[i][j]);
+            }
+            else{
+                var cuadro = document.createElement("cuadro");
+                    cuadro.classList.add("recuadroBlanco");
+                    fila.appendChild(cuadro);
+                console.log('fila impar recuadro blanco '+arregloTablero[i][j]);
+            }
+        }       
+    } 
     tablero.appendChild(fila);
 }
+console.log(arregloTablero[0][0])
