bfaa5ae05825399875764480b8dc7ec5f1ab3e39
Gonzalo De Castro <gonzalodecastro1@gmail.com>
2021-06-09 00:25:14 -0300

    Cuando seleccionamos alguna ficha de un color distinto o un casillero vacio el tablero se pintará de un color distinto para toda sus mismas clases

--- js/app.js
+++ js/app.js
@@ -45,17 +45,90 @@ for (var m = 0; m < arregloTablero.length; m++) {
             piezaBlanca.alt = 'Pieza_Blanca';
             piezaBlanca.className = 'fichas';
             document.getElementById('fila' + m +'columna' + n).appendChild(piezaBlanca);
+            document.getElementById('fila' + m +'columna' + n).addEventListener('click', buttonClick);
+            function buttonClick(){                
+                for (var m = 0; m < arregloTablero.length; m++) {  
+                    for (var n = 0; n < arregloTablero.length; n++) { 
+                        //Ficha Blanca corresponde a 1
+                        if (arregloTablero[m][n] === 1) {
+                            selecciona = document.getElementById('fila' + m +'columna' + n);
+                            selecciona.style.backgroundColor = 'green';
+                        }
+                        
+                    }
+                }
+            
+            }
+            
         //Ficha Roja corresponde a 2
         }else if (arregloTablero[m][n] === 2){
             var piezaRoja = document.createElement('img');
             piezaRoja.src = 'imgs/PiezaRoja.png';
             piezaRoja.alt = 'Pieza_Roja';
             piezaRoja.className = 'fichas';
-            document.getElementById('fila' + m +'columna' + n).appendChild(piezaRoja);     
+            document.getElementById('fila' + m +'columna' + n).appendChild(piezaRoja);
+            document.getElementById('fila' + m +'columna' + n).addEventListener('click', buttonClick);
+            function buttonClick(){               
+                for (var m = 0; m < arregloTablero.length; m++) {  
+                    for (var n = 0; n < arregloTablero.length; n++) { 
+                        if (arregloTablero[m][n] === 2){
+                            selecciona = document.getElementById('fila' + m +'columna' + n);
+                            selecciona.style.backgroundColor = 'red';    
+                        }
+                        
+                    }
+                }
+            
+            }
         }
+        else if (arregloTablero[m][n] === 0){
+            var casillaVacia = document.createElement('img');
+            casillaVacia.src = 'imgs/nada.png';
+            casillaVacia.alt = 'nada';
+            casillaVacia.className = 'fichas';
+            document.getElementById('fila' + m +'columna' + n).appendChild(casillaVacia);
+            document.getElementById('fila' + m +'columna' + n).addEventListener('click', buttonClick);
+            function buttonClick(){                
+                for (var m = 0; m < arregloTablero.length; m++) {  
+                    for (var n = 0; n < arregloTablero.length; n++) { 
+                        if (arregloTablero[m][n] === 0){
+                            selecciona = document.getElementById('fila' + m +'columna' + n);
+                            selecciona.style.backgroundColor = 'blue';    
+                        }
+                        
+                    }
+                }
+            
+            }     
+        }
+        
     }
 }
 
 
 console.log(arregloTablero[7][0])
 arregloTablero[7][0] = '5';
+
+
+
+/*
+document.getElementById('fila5columna0').addEventListener('click', buttonClick);
+document.querySelectorAll('.recuadroNegro[0][1]').addEventListener('click', buttonClick1);
+
+function buttonClick(){
+    selecciona = document.getElementById('fila5columna0');
+    selecciona.style.backgroundColor = 'green';
+
+}
+function buttonClick1(){
+    selecciona = document.getElementById('fila7columna0');
+    selecciona.style.backgroundColor = 'red';
+
+}
+
+/*
+var fichaMovil = document.getElementsByClassName('fichas').addEventListener('click', seleccion);
+
+function seleccion(){
+        fichaMovil.style.backgrounColor = '#000';
+} */
