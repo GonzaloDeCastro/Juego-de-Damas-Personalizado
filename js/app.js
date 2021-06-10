b59dff37280c4f0237a8ed8d12b27f5b03b8e30b
Gonzalo De Castro <gonzalodecastro1@gmail.com>
2021-06-09 22:42:51 -0300

    Ahora se puede seleccionar cada ficha y la misma queda pintada de manera individual

--- js/app.js
+++ js/app.js
@@ -45,20 +45,6 @@ for (var m = 0; m < arregloTablero.length; m++) {
             piezaBlanca.alt = 'Pieza_Blanca';
             piezaBlanca.className = 'fichas';
             document.getElementById('fila' + m +'columna' + n).appendChild(piezaBlanca);
-            document.getElementById('fila' + m +'columna' + n).addEventListener('click', buttonClick);
-            function buttonClick(){                
-                for (var m = 0; m < arregloTablero.length; m++) {  
-                    for (var n = 0; n < arregloTablero.length; n++) { 
-                        //Ficha Blanca corresponde a 1
-                        if (arregloTablero[m][n] === 1) {
-                            selecciona = document.getElementById('fila' + m +'columna' + n);
-                            selecciona.style.backgroundColor = 'green';
-                        }
-                        
-                    }
-                }
-            
-            }
             
         //Ficha Roja corresponde a 2
         }else if (arregloTablero[m][n] === 2){
@@ -67,68 +53,28 @@ for (var m = 0; m < arregloTablero.length; m++) {
             piezaRoja.alt = 'Pieza_Roja';
             piezaRoja.className = 'fichas';
             document.getElementById('fila' + m +'columna' + n).appendChild(piezaRoja);
-            document.getElementById('fila' + m +'columna' + n).addEventListener('click', buttonClick);
-            function buttonClick(){               
-                for (var m = 0; m < arregloTablero.length; m++) {  
-                    for (var n = 0; n < arregloTablero.length; n++) { 
-                        if (arregloTablero[m][n] === 2){
-                            selecciona = document.getElementById('fila' + m +'columna' + n);
-                            selecciona.style.backgroundColor = 'red';    
-                        }
-                        
-                    }
-                }
-            
-            }
-        }
-        else if (arregloTablero[m][n] === 0){
-            var casillaVacia = document.createElement('img');
-            casillaVacia.src = 'imgs/nada.png';
-            casillaVacia.alt = 'nada';
-            casillaVacia.className = 'fichas';
-            document.getElementById('fila' + m +'columna' + n).appendChild(casillaVacia);
-            document.getElementById('fila' + m +'columna' + n).addEventListener('click', buttonClick);
-            function buttonClick(){                
-                for (var m = 0; m < arregloTablero.length; m++) {  
-                    for (var n = 0; n < arregloTablero.length; n++) { 
-                        if (arregloTablero[m][n] === 0){
-                            selecciona = document.getElementById('fila' + m +'columna' + n);
-                            selecciona.style.backgroundColor = 'blue';    
-                        }
-                        
-                    }
-                }
-            
-            }     
         }
         
     }
 }
 
 
-console.log(arregloTablero[7][0])
-arregloTablero[7][0] = '5';
+var fichasBlancas = document.querySelectorAll('img[alt="Pieza_Blanca"]');
 
-
-
-/*
-document.getElementById('fila5columna0').addEventListener('click', buttonClick);
-document.querySelectorAll('.recuadroNegro[0][1]').addEventListener('click', buttonClick1);
-
-function buttonClick(){
-    selecciona = document.getElementById('fila5columna0');
-    selecciona.style.backgroundColor = 'green';
-
-}
-function buttonClick1(){
-    selecciona = document.getElementById('fila7columna0');
-    selecciona.style.backgroundColor = 'red';
+for (var h = 0; h<fichasBlancas.length; h++){
+    const fichaSeleccionadaBlanca = fichasBlancas[h];
+    fichaSeleccionadaBlanca.addEventListener('click', () => {
+    fichaSeleccionadaBlanca.style.boxShadow = '#A9F5F2 5px 5px 16px 10px';
+    });
 
 }
 
-/*
-var fichaMovil = document.getElementsByClassName('fichas').addEventListener('click', seleccion);
+var fichasRojas = document.querySelectorAll('img[alt="Pieza_Roja"]');
 
-function seleccion(){
-        fichaMovil.style.backgrounColor = '#000';
-} */
+for (var h = 0; h<fichasRojas.length; h++){
+    const fichaSeleccionadaRoja = fichasRojas[h];
+    fichaSeleccionadaRoja.addEventListener('click', () => {
+    fichaSeleccionadaRoja.style.boxShadow = '#A9F5F2 5px 5px 16px 10px';
+    });
+
+}
